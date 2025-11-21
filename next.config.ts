982bbcac8b0c768bc79f9ecdf2f1f4ce5a696b38
Felipe Imperio Alves de Nazareth <felipeimperio.imperioo@gmail.com>
2025-11-20 21:57:16 -0800

    fix: disable turbopack for webpack prisma plugin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const config: NextConfig = {
+  // Add empty turbopack config to silence error
+  turbopack: {},
+  
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
